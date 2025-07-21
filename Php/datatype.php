d19b4a37d5e92f55269487cd85b6d4fdc7483657
erlan <noref.kyoref@gmail.com>
2025-07-21 11:51:21 +0700

    php

---
+++ Php/datatype.php
@@ -0,0 +1,20 @@
+<?php
+//INTEGER
+$n = 2321;
+//String
+$x = "Hello dark Smile";
+$y= "PHP LAMBO";
+//Boolean true or false
+$m = true;
+//ARRAY
+$game = array("Ninja","ELden Ring","GTA");
+var_dump($x);
+echo ("<p><h1>");
+var_dump($y);
+echo ("<p>");
+var_dump($n);
+echo ("<p>");
+var_dump($m);
+echo ("<p>");
+var_dump($game);
+?>
