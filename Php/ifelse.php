542541a8175c83743197393a8eec64e9e9193854
erlan <noref.kyoref@gmail.com>
2025-07-22 21:02:34 +0700

    ifelse php

---
+++ Php/ifelse.php
@@ -0,0 +1,22 @@
+<?php
+// if else
+$name= "Erlan";
+$age = 25;
+if ($age > 24) {
+  echo("$name");
+} else {
+  echo("error");
+}
+// if inside if
+$name1 = "erlan";
+$name2 =   "shlypy";
+$number = "23";
+if ($number ==23 ) {
+  echo("<p>$name1 like $name2</p>");
+if ($number < 26) {
+   echo("<p>$name2 like $name1</p>");
+}else {
+  echo("$name2 get rejected  by $name1");
+}
+} 
+?>
