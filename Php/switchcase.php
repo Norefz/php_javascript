6d7a8143de98dbe2c49c547e06431bb9eabe4694
erlan <noref.kyoref@gmail.com>
2025-07-22 21:22:44 +0700

    switch

---
+++ Php/switchcase.php
@@ -0,0 +1,24 @@
+<?php 
+
+$month = "august";
+$name1 = "kanye west";
+$name2 = "kanye Easst";
+switch ($month) {
+    case 'august':
+        echo("$name1 have great day");
+        break;
+    case 'June':
+        echo("$name1 have great bad day");
+        break;
+case 'Jully':
+        echo("$name2 have great great day");
+        break;
+        case 'January':
+        echo("$name2 have great bad day");
+        break;
+    
+    default:
+        echo("all mr west have bad and great day");
+        break;
+}
+?>
