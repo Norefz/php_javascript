7d73cb19c4be1a22a74ab273abe6c6971628e8a0
erlan <noref.kyoref@gmail.com>
2025-07-31 18:57:30 +0700

    fornm php

---
+++ Php/formlogin.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Form Login Php</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous"> 
+
+    <style>
+      body{
+        background-imae:url()
+      }
+      .form-control{
+        
+      }
+    </style>
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body class= "  min-vh-100  align-items-center d-flex justify-content-center bg-dark">
+
+    
+    <div class="card "  style="width:30rem;" >
+      <h1 class="text-center mt-1 ">Login</h1>
+     <form action="POST" class="ms-3  me-3 ">
+  <div class="mb-3 mt-3 ">
+  <label for="exampleFormControlInput1" class="form-label">Username</label>
+  <input type="text" class="form-control" id="exampleFormControlInput1" >
+</div>
+<div class="mb-3">
+<label for="exampleFormControlInput1" class="form-label">Password</label>
+<input type="password" class="form-control" id="exampleFormControlInput1" >
+</div>
+<button class="btn btn-primary ms-1" style="width:27rem;">LOGIN</button>
+<btn class="text-primary text-center mt-4">Register</btn>
+</form>
+ </div>
+      </body>
+</html>
