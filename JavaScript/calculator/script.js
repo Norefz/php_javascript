fa605361f05253f01aa61f1da82f978f35afd422
erlan <noref.kyoref@gmail.com>
2025-10-09 23:18:25 +0700

    add function

---
+++ JavaScript/calculator/script.js
@@ -0,0 +1,10 @@
+const display = document.getElementById("screen");
+function CalcDisplay(input) {
+  display.value += input;
+}
+function ClearDisplay (input) {
+  display.value ="";
+}
+function name(params) {
+  
+}
