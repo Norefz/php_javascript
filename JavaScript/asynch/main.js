3df3827d0bf93506f43f2fa61e02a416051d86e4
erlan <noref.kyoref@gmail.com>
2025-10-13 22:00:55 +0700

    I love You For 1000 year

--- JavaScript/asynch/main.js
+++ JavaScript/asynch/main.js
@@ -11,18 +11,65 @@
 
 // Everything after Await is placed in an event queue
 
+// function First() {
+//   return new Promise ((resolve,reject)=>{
+//   setTimeout(() => {
+//     console.log("I")
+//     document.getElementById("i").innerHTML = "I";
+//   }, 500);
+//   });
+// }
+// function Second(){
+//   return new Promise ((resolve,reject)=>{
+//   setTimeout(() => {
+//      console.log("Love")
+//        
+//     document.getElementById("love").innerHTML = "Love";
+//   }, 800);
+//   });
+// }
+//
+// function Third(){
+//   return new Promise ((resolve,reject)=>{
+//   setTimeout(() => {
+//      console.log("You")
+//        
+//     document.getElementById("you").innerHTML = "You";
+//   }, 1000);
+//   });
+// }
+// function Four(){
+//   return new Promise ((resolve,reject)=>{
+//   setTimeout(() => {
+//      console.log("For")
+//        
+//     document.getElementById("for").innerHTML = "For";
+//   }, 1300);
+//   });
+// }
+// function Five(){
+//   return new Promise ((resolve,reject)=>{
+//   setTimeout(() => {
+//      console.log("1000 year")
+//     document.getElementById("thousand").innerHTML = "1000 year";
+//   }, 1700);
+//   });
+// }
 function First() {
   return new Promise ((resolve,reject)=>{
   setTimeout(() => {
     console.log("I")
-  }, 500);
+    document.getElementById("i").innerHTML = "I";
+  }, 1000);
   });
 }
 function Second(){
   return new Promise ((resolve,reject)=>{
   setTimeout(() => {
      console.log("Love")
-  }, 800);
+       
+    document.getElementById("i").innerHTML = "Love";
+  }, 2000);
   });
 }
 
@@ -30,25 +77,39 @@ function Third(){
   return new Promise ((resolve,reject)=>{
   setTimeout(() => {
      console.log("You")
-  }, 1000);
+       
+    document.getElementById("i").innerHTML = "You";
+  }, 3000);
   });
 }
 function Four(){
   return new Promise ((resolve,reject)=>{
   setTimeout(() => {
      console.log("For")
-  }, 1300);
+       
+    document.getElementById("i").innerHTML = "For";
+  }, 4000);
   });
 }
 function Five(){
   return new Promise ((resolve,reject)=>{
   setTimeout(() => {
      console.log("1000 year")
-  }, 1700);
+    document.getElementById("i").innerHTML = "1000 year";
+  }, 5000);
   });
 }
+function Final(){
+  return new Promise((resolve,reject)=> {
+    setTimeout(() => {
+   document.getElementById("i").innerHTML = "I Love You For 1000 year:)"
+    }, 7000);
+  } );
+}
+
     First();
     Second();
     Third();
     Four();
     Five();
+    Final();
