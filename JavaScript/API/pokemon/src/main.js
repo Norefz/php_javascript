ef96caf6c28f330e376a95dcd2f1a0599f2c1d41
erlan <noref.kyoref@gmail.com>
2025-10-20 01:49:58 +0700

    fetch done

--- JavaScript/API/pokemon/src/main.js
+++ JavaScript/API/pokemon/src/main.js
@@ -1,4 +1,9 @@
 fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-       .then(response => response.json())
+       .then(response => {
+            if (!response.ok) {
+              throw new Error("coulnt fetch resource");
+            }
+        return response.json()
+       } )
        .then(data => console.log(data.name))
       .catch(error => console.error(error));
