8aca0f4ea2a3d6aa2279b2fed23fafbcf8adaa10
erlan <noref.kyoref@gmail.com>
2025-10-20 01:41:03 +0700

    create response with pokemon api

--- JavaScript/API/pokemon/src/main.js
+++ JavaScript/API/pokemon/src/main.js
@@ -1,1 +1,4 @@
-const pokemo
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+       .then(response => response.json())
+       .then(data => console.log(data.name))
+      .catch(error => console.error(error));
